cdacbb52a820dee2e4b0d1a0b3542d3f408c5b18
mest2011 <mest2011mest@gmail.com>
2021-05-24 23:17:04 -0300

    Cleaning up code

--- src/arduinoCommunicator.js
+++ src/arduinoCommunicator.js
@@ -8,34 +8,67 @@ const si = require("systeminformation");
 
 port.pipe(parser);
 parser.on("data", console.log);
-function write() {
-  port.on("data", async function (data) {
-    let cpuUsage = parseInt(
+
+const getCPUData = async () => {
+  try {
+    return await parseInt(
       await si.currentLoad().then((data) => {
         return data.currentLoad;
       })
     );
-    let memUsage = await si.mem().then((data) => {
+  } catch (error) {
+    return Promise.reject("Oops!").catch((err) => {
+      throw new Error(err);
+    });
+  }
+};
+
+const getMemoryData = async () => {
+  try {
+    return await si.mem().then((data) => {
       return parseInt((data.used * 100) / data.total);
     });
-    let bateryPercent = await si.battery().then((data) => {
+  } catch (error) {
+    return Promise.reject("Oops!").catch((err) => {
+      throw new Error(err);
+    });
+  }
+};
+
+const getBateryData = async () => {
+  try {
+    return await si.battery().then((data) => {
       return data;
     });
+  } catch (error) {
+    return Promise.reject("Oops!").catch((err) => {
+      throw new Error(err);
+    });
+  }
+};
+
+const stringFactory = () => {
+  port.on("data", async (data) => {
+    let cpuUsage = await getCPUData();
+    let memUsage = await getMemoryData();
+    let bateryPercent = await getBateryData();
+
+    let str =
+      `CPU:${cpuUsage < 10 ? "0" + cpuUsage : cpuUsage}% ` +
+      `Mem:${memUsage}% ` +
+      `Bat:${
+        bateryPercent.percent < 10
+          ? "0" + bateryPercent.percent
+          : bateryPercent.percent
+      }% ` +
+      `${bateryPercent.acConnected ? "charging" : "        "} `;
 
     if (cpuUsage) {
-      port.write(
-        `CPU:${
-          cpuUsage < 10 ? "0" + cpuUsage : cpuUsage
-        }% Mem:${memUsage} %Bat:${
-          bateryPercent.percent < 10
-            ? "0" + bateryPercent.percent
-            : bateryPercent.percent
-        }% ${bateryPercent.acConnected ? "charging": "        "} `
-      );
+      port.write(str);
     }
   });
-}
+};
 
-port.on("open", function () {
-  write();
+port.on("open", () => {
+  stringFactory();
 });
